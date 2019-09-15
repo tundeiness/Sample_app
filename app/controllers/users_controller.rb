6f06223b5932fc0d3f1f102b3d8e5e3f8a35fd27
tundeiness <tundeiness@yahoo.co.uk>
2019-09-15 18:38:20 +0100

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -8,6 +9,9 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 
 end
